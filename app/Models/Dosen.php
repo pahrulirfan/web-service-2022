3c063435bae157b598f879acebef19c5e2ab45fe
Pahrul Irfan <ip.5090@gmail.com>
2022-05-17 15:20:46 +0800

    Setting model

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -9,9 +9,16 @@ class Dosen extends Model
 {
     use HasFactory;
 
+    // jika nama tabel tidak sesuai dengan aturan laravel, maka perlu dituliskan nama tabelnya
+    // nama tabel yg sesuai untuk model Dosen yaitu dosens
     protected $table = 'dosen';
 
-//    public $timestamps = false;
+    // jika kolom primary key bukan id (default nama kolom nya id)
+    // maka perlu dituliskan nama kolom yg menjadi primary key
+    protected $primaryKey = 'id';
+
+    // pilihan jika tidak ingin menyimpan timestamp pembuatan
+    public $timestamps = true
 
     protected $fillable = ['nik', 'nama_dosen', 'umur'];
 }
