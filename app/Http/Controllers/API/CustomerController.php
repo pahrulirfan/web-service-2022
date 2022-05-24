0443a6b729e7e75957e55a180ec3f00e36e91c70
Pahrul Irfan <ip.5090@gmail.com>
2022-05-24 15:44:30 +0800

    api customer

---
+++ app/Http/Controllers/API/CustomerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $data = Customer::all();
+        return response()->json($data, 200);
+    }
+
+    public function show(Customer $id)
+    {
+        return response()->json($id, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('v1/customers', [CustomerController::class, 'index']);
+Route::get('v1/customer/{id}', [CustomerController::class, 'show']);
